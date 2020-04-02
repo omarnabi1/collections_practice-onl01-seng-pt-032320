023e1b9cab83acf5bcc48ea1ad51a872526093fe
Omar Nabi <onabi20@gmail.com>
2020-04-02 03:10:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,3 +27,15 @@ def reverse_array (integer)
   new_array = integer.reverse
   new_array
 end
+
+def kesha_maker(array)
+  kesha = [ ] 
+  array.each do |word|
+    word_array = word.split ""
+    word_array[2] = "$"
+    kesha << word_array.join
+  end
+  kesha
+end
+
+  
