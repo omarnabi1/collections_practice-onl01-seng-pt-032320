38d697f98c69c1445779ecdf4b9d1cf64d79d646
Omar Nabi <onabi20@gmail.com>
2020-04-02 03:05:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,3 +22,8 @@ def swap_elements_from_to(array, index, destination_index)
   array[index], array[destination_index] = array[destination_index], array[index]
   return array
 end
+
+def reverse_array (array)
+  new_array = integers.reverse
+  new_array
+end
